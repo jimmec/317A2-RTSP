1a63d376e3b71793d7433b59f618ef0b4ec9a9b2
Jimmy Cao <jimmec@gmail.com>
2014-11-02 14:24:15 -0800

    rough stat tracking api done

--- src/ubc/cs317/rtsp/client/model/SessionStat.java
+++ src/ubc/cs317/rtsp/client/model/SessionStat.java
@@ -1,6 +1,8 @@
 package ubc.cs317.rtsp.client.model;
 
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Tracks all playback statistics related to a single RTSP session.
@@ -9,11 +11,51 @@ import java.util.Date;
  *
  */
 public class SessionStat {
+   private Timer timer;
    public String id;
+   public String videoName;
    public Date startTime;
    public Date endTime;
    public long playbackLength;
    public long framesPlayed;
    public long framesLost;
    public long framesOutOfOrder;
+   public int cseq;
+
+   public SessionStat(String id, String vidName) {
+      this.id = id;
+      videoName = vidName;
+      startTime = new Date();
+      timer = new Timer();
+   }
+
+   /**
+    * Start a timer to update the time video has actually been playing via 10ms intervals
+    */
+   public void startPlay() {
+      timer.schedule(new TimerTask() {
+         @Override
+         public void run() {
+            playbackLength += 10;
+         }
+      }, 10);
+   }
+
+   /**
+    * Pause/stops the playback timer.
+    */
+   public void pausePlay() {
+      timer.cancel();
+   }
+
+   /**
+    * Must be called when the current session is finished.
+    * ie. when teardown() is called in RTSPConnection
+    */
+   public void finalize() {
+      timer.cancel();
+      this.endTime = new Date();
+      timer = null;
+   }
+
 }
--- src/ubc/cs317/rtsp/client/net/RTSPConStats.java
+++ src/ubc/cs317/rtsp/client/net/RTSPConStats.java
@@ -1,15 +1,78 @@
 package ubc.cs317.rtsp.client.net;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ubc.cs317.rtsp.client.model.SessionStat;
 
 /**
- * This class tracks playback statistics of an entire RTSP connection. And produces stats on them.
+ * This class tracks playback statistics of an entire RTSP connection,
+ * organized by each individual session. <br/>
+ * Each time a newSession is created, all stats tracking is done against the latest session.
  * 
  * @author jimmy
  *
  */
 public class RTSPConStats {
    List<SessionStat> sessions;
+
+   public RTSPConStats() {
+      sessions = new ArrayList<SessionStat>();
+   }
+
+   /**
+    * Start tracking stats for a new RTSP session.
+    * MUST be called before using any other methods to track this session.
+    * Should be called each time setup() is called in RTSPConnection.
+    * 
+    * @param id
+    *           sessionId as returned by the RTSP server for the current session to be tracked
+    * @param vidName
+    *           name of the video file being requested for this session.
+    */
+   public void newSession(String id, String vidName) {
+      sessions.add(new SessionStat(id, vidName));
+   }
+
+   public void endSession() {
+      latest().finalize();
+   }
+
+   public void playStart() {
+      latest().startPlay();
+   }
+
+   public void playPause() {
+      latest().pausePlay();
+   }
+
+   public void newRequest() {
+      latest().cseq++;
+   }
+
+   public void framePlayed() {
+      latest().framesPlayed++;
+   }
+
+   public void frameLost() {
+      latest().framesLost++;
+   }
+
+   public void frameOutOrder() {
+      latest().framesOutOfOrder++;
+   }
+
+   private SessionStat latest() {
+      if (sessions.size() < 1) {
+         throw new IllegalStateException();
+      }
+      return sessions.get(sessions.size());
+   }
+
+   /**
+    * Print out a summary of all recorded stats so far.
+    */
+   public void report() {
+
+   }
 }
